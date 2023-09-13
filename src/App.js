98aaca94a2dae6c088351fb8bd7fb53344279b94
Abhi Mondal <85216013+AbhiMondal20@users.noreply.github.com>
2023-09-13 20:21:01 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <>
-      saaaaaaaaaaaHello    
-      <h1>
-        Hello World
-      </h1>
-      <h1>
-        Hello World2
-      </h1>
-    </>
+    <div className="App">
+      <h2>Wellcome To React Redux</h2>
+      <input type="text" className="control-form" name="" placeholder="" /><br/>
+      <button className="btn btn-primary mb-2">+</button>
+      <button className="btn btn-danger mb-2">-</button>
+    </div>
   );
 }
 
